609f99c13863a26f3a3ab9039288e4abb0047a51
Mike Dragert <mikedragert@gmail.com>
2024-03-17 22:58:28 -0400

    Switch sold out logic into Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
+
+  def soldOut
+    quantity == 0
+  end
 end
